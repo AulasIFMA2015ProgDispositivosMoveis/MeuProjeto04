77d01913cdb683161937c27eac8f2a0ddc94c2d9
Mauro Silva <maurosilva@ifma.edu.br>
2015-04-05 10:17:23 -0300

    Inclusão da Caixa de Diálogo.

--- MeuProjeto04/src/com/aula/meuprojeto04/Principal.java
+++ MeuProjeto04/src/com/aula/meuprojeto04/Principal.java
@@ -1,17 +1,42 @@
 package com.aula.meuprojeto04;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class Principal extends ActionBarActivity {
 
+	private Spinner cmbSabores;
+	private RadioGroup rgbFormasdePagamento;
+	private CheckBox chkRefri, chkBordas;
+	private Button btnEnviar;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        //criação do Array que irá conter os sabores
+        String sabores[] = {"Jaca","Buriti","Calabresa","Farinha D'agua"};
+        //Criação do ArrayAdapter que será adicionado ao Spinner
+        ArrayAdapter<String> meuadapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sabores);
+        //Recupera os componentes do XML
+        cmbSabores = (Spinner) findViewById(R.id.cmbSabores);
+        rgbFormasdePagamento = (RadioGroup) findViewById(R.id.rbgFormapag);
+        chkRefri = (CheckBox) findViewById(R.id.chkRefri);
+        chkBordas = (CheckBox) findViewById(R.id.chkBorda);
+        btnEnviar = (Button) findViewById(R.id.btnEnviar);
+        //Adiciona o ArrayAdapter ao Spinner
+        cmbSabores.setAdapter(meuadapter);
     }
 
 
@@ -33,4 +58,44 @@ public class Principal extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+    public void mostrarEscolha(View v){
+    	//Método chamado no click do Botão (ver XML de layout)
+    	
+    	String pizza,adicionais="",formapag="",msg;
+    	
+    	//Recupera o que foi selecionado pelo usuário na combo de sabores
+    	pizza = cmbSabores.getSelectedItem().toString();
+    	
+    	//Verifica qual CheckBox foi selecionado
+    	if (chkBordas.isChecked()) {
+			adicionais = adicionais + " Bordas ";
+		}
+    	
+    	if (chkRefri.isChecked()) {
+    		adicionais = adicionais + " Refrigerante ";
+		}
+    	
+    	//Verifica qual Radio foi selecionado
+    	switch (rgbFormasdePagamento.getCheckedRadioButtonId()) {
+			case R.id.rdDinheiro:
+				formapag = "Em Dinheiro";
+				break;
+			case R.id.rdCartao:
+				formapag = "Em Cartão";
+				break;
+		}
+    	
+    	msg = "Sabor Escolhido:" + pizza + "\n";
+    	msg = msg + "Adicionais:" + adicionais + "\n";
+    	msg = msg + "Forma de Pagamento:" + formapag;
+    	
+    	AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+    	alerta.setTitle("Pizza de SI");
+    	alerta.setMessage(msg);
+    	alerta.setPositiveButton("Ok", null);
+    	alerta.show();
+    	//Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+    
 }
